915772a224697563a2a7b51d01d1e53b50fea624
Milo Sanders <admin@autumnomous.com>
2023-07-16 20:32:36 -0400

    adding typeorm and sqlite for development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [],
+    synchronize: true // remove for production
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
