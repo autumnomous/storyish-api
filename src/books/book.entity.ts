a90fe3f75e08ab157e0392e191f95a190798af0b
Milo Sanders <admin@autumnomous.com>
2023-07-16 20:43:01 -0400

    progress on setting up book entity

--- src/books/book.entity.ts
+++ src/books/book.entity.ts
@@ -3,4 +3,17 @@ import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
 @Entity()
 export class Book {
     
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    title:string;
+
+    // have a separate table for creators to be 
+    // used across different media types
+    // @Column()
+    // author:string;
+
+    @Column()
+    isbn:string;
 }
