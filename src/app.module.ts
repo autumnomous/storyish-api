899802f91a4981d2736c12aacae47e5f2f6ed770
Milo Sanders <admin@autumnomous.com>
2023-07-17 23:02:21 -0400

    adding Creator to DB Entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { CreatorsModule } from './creators/creators.module';
+import { Creator } from './creators/creator.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [Creator],
     synchronize: true // remove for production
   }), CreatorsModule],
   controllers: [AppController],
